8bb9bcb8000b6a8ff2fc839a4c9c8ca4f1fcc072
emirsennin <n19238816@cs.hacettepe.edu.tr>
2020-11-26 23:15:46 +0300

    add k smoothing added

--- hw.py
+++ hw.py
@@ -45,7 +45,7 @@ def unigramFinder(sentenceTokens,wordCount):
 
     return unigramDf
 
-def bigramFinder(sentenceTokens,unigramDf):
+def bigramFinder(sentenceTokens,unigramDf,k,unique_vocab):
     bigramList = []
     for sentence in sentenceTokens:
         tokenizer = RegexpTokenizer(r'\w+')
@@ -72,12 +72,18 @@ def bigramFinder(sentenceTokens,unigramDf):
                 unigramDf.loc[unigramDf["Text"] == bigramDf.Text[0].split()[1]].Probability.item()
         else:
             bigramDf.iloc[index, bigramDf.columns.get_loc('BiProbability')] = \
-                row.Count/ unigramDf.loc[unigramDf["Text"] == bigramDf.Text[index].split()[0]].Count.item()
+                (row.Count + k) / (unigramDf.loc[unigramDf["Text"] == bigramDf.Text[index].split()[0]].Count.item() + (k * unique_vocab))
 
     # bigramProbality = ikilinin unigram countu/ilk kelimenin unigram countu
     return bigramDf
 
+def addKsmoothing(df):
+    pass
 
+def makeLowestUnknown(data,unigramDf):
+    replaces = unigramDf.sort_values(by="Probability",ascending=False).Text.values[-3:]
+    newData = data.replace(replaces[0],"UNK").replace(replaces[1],"UNK").replace(replaces[2],"UNK")
+    return newData
 def generalFunc(path,name):
     print("\n\n--------- Starting To Work For "+name+"-------------\n\n")
     file = open(path)
@@ -93,9 +99,27 @@ def generalFunc(path,name):
 
     unigramDf = unigramFinder(sentenceTokens,len(wordTokens))
 
-    bigramDf = bigramFinder(sentenceTokens,unigramDf)
+    bigramDf = bigramFinder(sentenceTokens,unigramDf,0,0)
     print(bigramDf.sort_values(by="BiProbability", ascending=False))
     print(bigramDf.head())
+
+    data_with_unknown = makeLowestUnknown(data,unigramDf)
+
+    sentenceTokensWitUnknown = sentenceTokenize(data_with_unknown)
+
+    wordTokensWithUnknown = wordTokenize(data_with_unknown)
+
+    filtered_word_freq_unknown = freqFinder(wordTokensWithUnknown)
+
+    unigramDfWithUnknown = unigramFinder(sentenceTokensWitUnknown,len(wordTokensWithUnknown))
+
+    bigramDfWithUnknown = bigramFinder(sentenceTokensWitUnknown,unigramDfWithUnknown,0,0)
+    print(bigramDfWithUnknown.sort_values(by="BiProbability", ascending=False))
+    print(bigramDfWithUnknown.head())
+
+    smoothedBigramDf = bigramFinder(sentenceTokensWitUnknown,unigramDfWithUnknown,0.5,len(filtered_word_freq_unknown))
+    print(smoothedBigramDf.sort_values(by="BiProbability", ascending=False))
+    print(smoothedBigramDf.head())
 
     print("\n\n--------- Finished Working For "+name+"-------------\n\n")
 
